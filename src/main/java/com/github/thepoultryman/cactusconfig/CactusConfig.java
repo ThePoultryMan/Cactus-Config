65296ef9aed10cc3595778d39d91d1f628c635f0
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-17 20:53:37 -0400

    Change startup message

--- src/main/java/com/github/thepoultryman/cactusconfig/CactusConfig.java
+++ src/main/java/com/github/thepoultryman/cactusconfig/CactusConfig.java
@@ -5,11 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CactusConfig implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "cactusconfig";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		LOGGER.info("Cactus Config is being initialized.");
 	}
 }
