ac8d04f3df1e7ad3386e574f0af0c56147308613
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-17 22:46:54 -0400

    Add universal option setting and getting

--- src/main/java/io/github/thepoultryman/cactusconfig/ConfigManager.java
+++ src/main/java/io/github/thepoultryman/cactusconfig/ConfigManager.java
@@ -10,10 +10,28 @@ public abstract class ConfigManager {
     public ConfigManager(String fileName) {
         this.fileName = fileName;
         this.config = FileConfig.builder(FabricLoader.getInstance().getConfigDir() + "/" + this.fileName + ".toml")
-                .defaultResource("/" + this.fileName + ".toml").autosave().build();
+                .defaultResource("/" + this.fileName + ".toml").build();
     }
 
     public void loadConfig() {
         this.config.load();
+    }
+
+    public void setConfigOption(String optionPath, Object optionValue) {
+        if (this.config.contains(optionPath)) {
+            this.config.set(optionPath, optionValue);
+            this.config.save();
+        } else {
+            CactusConfig.LOGGER.warn("The path '" + optionPath + "' does not exist, and therefore it cannot be set.");
+        }
+    }
+
+    public Object getConfigOption(String optionPath) {
+        if (this.config.contains(optionPath)) {
+            return this.config.get(optionPath);
+        } else {
+            CactusConfig.LOGGER.warn("The path '" + optionPath + "' does not exist, and therefore it cannot be gotten.");
+            return null;
+        }
     }
 }
