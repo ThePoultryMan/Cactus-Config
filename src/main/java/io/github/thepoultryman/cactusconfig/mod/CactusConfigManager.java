38086d7ad57462f0ff40eb739a870987ba7e8df3
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-19 16:41:04 -0400

    Save option to file

--- src/main/java/io/github/thepoultryman/cactusconfig/mod/CactusConfigManager.java
+++ src/main/java/io/github/thepoultryman/cactusconfig/mod/CactusConfigManager.java
@@ -7,7 +7,7 @@ import net.minecraft.text.TranslatableText;
 public class CactusConfigManager extends ConfigManager {
     public final OptionHolder general = new OptionHolder(new TranslatableText("cactus_config.mod.tabs.general"),
             new TranslatableText("cactus_config.mod.tabs.desc.general"));
-    private boolean skipResetConfirmation;
+    public boolean skipResetConfirmation;
 
     public CactusConfigManager(String fileName) {
         super(fileName);
@@ -18,11 +18,16 @@ public class CactusConfigManager extends ConfigManager {
         super.loadConfig();
 
         this.getAndCreateBooleanOption(general, "mod.general.skip_reset_confirmation", false,
-                () -> this.skipResetConfirmation, b -> this.skipResetConfirmation = b, true);
+                () -> this.skipResetConfirmation, this::setSkipResetConfirmation, true);
     }
 
     @Override
     public boolean canReset() {
         return true;
+    }
+
+    private void setSkipResetConfirmation(boolean skipConfirmation) {
+        this.skipResetConfirmation = skipConfirmation;
+        this.setConfigOption("mod.general.skip_reset_confirmation", skipConfirmation);
     }
 }
